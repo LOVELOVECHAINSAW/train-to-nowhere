3b59fe1a1c05ef6bc53778059320456f04ee771f
Moowool <moowool195@gmail.com>
2021-11-14 15:56:00 -0500

    Added sprites and sound effects to door.

--- entities/Door.gd
+++ entities/Door.gd
@@ -3,7 +3,7 @@ extends Area2D
 enum rooms {ROOM1, ROOM2, ROOM3}
 
 export(rooms) onready var leads_to: int
-export(bool) var locked: bool
+export(bool) var locked: bool setget _set_locked
 
 
 func _on_Door_body_entered(_body) -> void:
@@ -12,3 +12,12 @@ func _on_Door_body_entered(_body) -> void:
 	else:
 # warning-ignore:return_value_discarded
 		get_tree().change_scene_to(Map.room[leads_to])
+
+func _set_locked(is_locked: bool) -> void:
+	if not is_locked:
+		$Sound.play()
+		$Sprite.frame = 1
+	else:
+		$Sprite.frame = 0
+	
+	locked = is_locked
