23929d5e37506c64ed2a3e7eeac6060fe0e8a4c4
Moowool <moowool195@gmail.com>
2021-11-13 21:52:17 -0500

    Removed useless scenes and scripts, updated project.godot. Created a placeholder scene as Level1.tscn.

---
+++ entities/Door.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+var leads_to: int
+var locked: bool
--- scenes/characters/Conductor.gd
+++
@@ -1,7 +0,0 @@
-extends "res://scenes/Character.gd"
-
-func _get_dialog(item):
-	var dialog =  Dialogic.start("Intro")
-	if item == Item.TYPE.Key:
-		dialog = Dialogic.start("Conductor Key")
-	return dialog
