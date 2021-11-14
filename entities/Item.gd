a40ad5e5c192d78c146e9850f0df9aa410a858be
Moowool <moowool195@gmail.com>
2021-11-13 21:41:18 -0500

    Wrote an item holding system.

--- entities/Item.gd
+++ entities/Item.gd
@@ -1,61 +1,16 @@
-extends Node
+extends Area2D
 
-class_name Item
+onready var sprite: Sprite = $Sprite
 
-enum TYPE {
-	Key,
-	Pipe
-}
+var item_name: String = "item"
 
-onready var player = get_tree().current_scene.get_node("Player")
-var previous_player_state
-var mouse_hovering = false
-var type
+func _on_Item_body_entered(body: Node):
+	if body.is_in_group("player"):
+		disconnect("body_entered", self, "_on_Item_body_entered")
+		get_parent().call_deferred("remove_child", self)
+		body.set_item(self)
 
-func _process(_delta):
-	var distance = self.position.distance_to(player.global_position) 
-	if distance < 150 and mouse_hovering:
-		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 
-# Override this function
-func on_combine(item_to_combine):
-	pass
-
-func on_dialog_end(timeline):
-	player.state = self.previous_player_state
-
-func on_click():
-	if player.dropping_item:
-		return
-
-	if player.held_item != null:
-		handle_combine()
-		return
-
-	player.on_item_clicked(self.type)
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-	var where = player.hovering_over.find(self)
-	player.hovering_over.remove(where)
-	queue_free()
-
-func handle_combine():
-	var dialog = on_combine(player.held_item)
-	dialog.connect("timeline_end", self, "on_dialog_end")
-	previous_player_state = player.state
-	player.state = player.STATE.TALKING
-	add_child(dialog)
-
-func _on_Area2D_mouse_entered():
-	mouse_hovering = true
-	player.hovering_over.push_front(self)
-
-func _on_Area2D_mouse_exited():
-	mouse_hovering = false
-	var where = player.hovering_over.find(self)
-	player.hovering_over.remove(where)
-	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
-
-func _on_Area2D_input_event(viewport:Node, event:InputEvent, shape_idx:int):
-	if event is InputEventMouseButton:
-		if self.global_position.distance_to(player.global_position) < 150 and event.is_pressed():
-			on_click()
+func _on_Item_body_exited(body):
+	connect("body_entered", self, "_on_Item_body_entered")
+	disconnect("body_exited", self, "_on_Item_body_exited")
--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -1,14 +1,28 @@
 extends KinematicBody2D
 
-
 const WALK_SPEED = 300
+
+onready var held_position: Vector2 = $HeldPosition.position
 
 var velocity := Vector2.ZERO
 var animation: String
-var held_item: int
+var item: Node setget set_item
+
+func _unhandled_key_input(_event):
+	if Input.is_action_just_pressed("ui_accept") and item:
+		_drop_item()
+
+func _drop_item() -> void:
+	$HeldItems.remove_child(item)
+	get_parent().call_deferred("add_child", item)
+
+	item.connect("body_exited", item, "_on_Item_body_exited")
+	item.global_position = self.global_position
+	item = null
 
 func _physics_process(_delta) -> void:
 	set_velocity()
+# warning-ignore:return_value_discarded
 	move_and_slide(velocity)
 
 	set_animation()
@@ -36,3 +50,14 @@ func set_animation() -> void:
 			animation = "walk_right"
 		_:
 			animation = "" # We can give a proper animation name later
+
+func set_item(new_item: Node) -> void:
+	item = new_item
+
+	for held_item in $HeldItems.get_children():
+		held_item.queue_free()
+
+	# Deferred because adding and removing children can't be done while
+	# flushing queries
+	$HeldItems.call_deferred("add_child", item)
+	item.call_deferred("set_position", held_position)
