f0cbe48605d0188f825cc8941a3508acf9ab2c28
Moowool <moowool195@gmail.com>
2021-11-14 19:47:30 -0500

    Made so the main menu leads to the first level.

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var scene: Resource = preload("res://scenes/Car1.tscn")
+var scene: Resource = Map.room[0]
 
 func _unhandled_key_input(event):
 	if event.scancode > 47 and event.scancode < 91: # If the key is a number or a letter
