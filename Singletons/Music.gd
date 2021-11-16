4ed59b731096464fc4fc675b48cf41ade0cd407a
Moowool <moowool195@gmail.com>
2021-11-16 03:27:10 -0500

    Changed music level before starting it.

--- Singletons/Music.gd
+++ Singletons/Music.gd
@@ -5,5 +5,5 @@ var music = AudioStreamPlayer.new()
 func _ready():
 	music.stream = preload("res://assets/audio/Train to Nowhere.ogg")
 	get_tree().root.call_deferred("add_child", music)
-	music.play()
 	music.volume_db = -20
+	music.play()
