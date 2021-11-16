76a19ffebd40e782a637726c7712aa56eebd0bcf
Moowool <moowool195@gmail.com>
2021-11-16 03:23:36 -0500

    Reworked containers and doors. Now supports multiple variants, and can be changed as exports in real-time in the editor.

--- entities/scripts/Container.gd
+++ entities/scripts/Container.gd
@@ -1,30 +1,50 @@
+tool
 extends Area2D
 
-var opened := false setget set_opened
+enum TYPE {SUITCASE = 0, LOCKER = 4, DRAWER = 8}
+enum TIME_OF_DAY {DAY = 0, NIGHT = 2}
+enum STATE {CLOSED, OPENED}
+
 var player: Node
+
 export(PackedScene) var containing_item: PackedScene
+# Setgets to trigger _update_sprite().
+# This function updates the sprite live, in the editor.
+export(bool) var opened := false setget _set_opened
+export(TYPE) var CONTAINER_TYPE := TYPE.SUITCASE setget _set_type
+export(TIME_OF_DAY) var VARIANT := TIME_OF_DAY.DAY setget _set_variant
 
-## Sets the visuals accordingly
-func set_opened(is_opened: bool) -> void:
-	$BubbleSprite.set_visible(not is_opened)
+func _ready():
+	print(1 + int(true))
 
-	if is_opened:
-		$ContainerSprite.frame = 1
-	else:
-		$ContainerSprite.frame = 0
-
-	opened = is_opened
+func _update_sprite() -> void:
+	$BubbleSprite.set_visible(not opened)
+	$ContainerSprite.frame = CONTAINER_TYPE + VARIANT + int(opened)
 
 func _unhandled_key_input(event):
-	if player and Input.is_action_just_pressed("ui_accept") and not opened:
+	# Spawns the held item
+	if player and Input.is_action_just_pressed("ui_accept")\
+			and not opened and containing_item:
 		var item = containing_item.instance()
 		get_parent().add_child(item)
-		item.set_position(self.global_position)
-		set_opened(true)
+		item.set_position(self.global_position + Vector2.DOWN) # Spawns in player's hand
+		_set_opened(true)
 
 func _on_Container_body_entered(body):
 	player = body
 
-
 func _on_Container_body_exited(body):
 	player = null
+
+func _set_opened(is_opened: bool) -> void:
+	opened = is_opened
+	_update_sprite()
+
+func _set_type(new_type: int) -> void:
+	CONTAINER_TYPE = new_type
+	_update_sprite()
+
+func _set_variant(new_variant: int) -> void:
+	VARIANT = new_variant
+	_update_sprite()
+
--- entities/scripts/Door.gd
+++ entities/scripts/Door.gd
@@ -1,10 +1,16 @@
+tool
 extends Area2D
 
 enum rooms {ROOM1, ROOM2, ROOM3}
+enum TIME_OF_DAY {DAY = 0, NIGHT = 2}
 
-export(rooms) onready var leads_to: int
-export(bool) var locked: bool setget _set_locked
+export(rooms) var leads_to: int # Refers to the room array in Map singleton
 export(bool) var requires_key: bool = false
+
+# Setgets to trigger _update_sprite().
+# This function updates the sprite live, in the editor.
+export(bool) var locked: bool setget _set_locked
+export(TIME_OF_DAY) var VARIANT setget _set_variant
 
 func _on_Door_body_entered(player) -> void:
 	if locked:
@@ -16,19 +22,27 @@ func _on_Door_body_entered(player) -> void:
 	if requires_key and player.item and locked:
 		if player.item.id == "key":
 			$OpenSound.play()
-
 			player.set_processes(false)
-
 			yield($OpenSound, "finished")
-
 # warning-ignore:return_value_discarded
 			get_tree().change_scene_to(Map.room[leads_to])
 
-func _set_locked(is_locked: bool) -> void:
-	if not is_locked:
+func _update_sprite():
+	if not locked:
 		$OpenSound.play()
-		$Sprite.frame = 1
-	else:
-		$Sprite.frame = 0
 
+	$Door.frame = VARIANT + int(locked)
+
+## locked setter.
+## Used to updated sprite in the editor
+func _set_locked(is_locked: bool) -> void:
 	locked = is_locked
+	# Call deferred because _set_locked gets called before ready
+	call_deferred("_update_sprite")
+
+## variant setter.
+## Used to updated sprite in the editor.
+func _set_variant(new_variant: int) -> void:
+	VARIANT = new_variant
+	# Call deferred because _set_locked gets called before ready
+	call_deferred("_update_sprite")
